6d3f6f1fdfee89e0663951c6605957ba0d0fe632
tbcs50play <155087499+tbcs50play@users.noreply.github.com>
2023-12-29 13:40:55 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import streamlit as sts
+from nicegui import ui
+
+#Welcome me to my app
+ui.chat_message('Hey Travis, Sweet app!',
+                name='Robot',
+                avatar='https://robohash.org/ui')
+
+#define function for declaring awesomeness and assigning button count
+click_count=int(0)
+
+def clicked():
+    ui.notify('You are awesome')
+    global click_count
+    click_count +=1
+    button.set_text(click_count)
+    
+#create the button
+button = ui.button('Click me!', on_click=clicked).style('width: 200px')
+
+ui.run()
